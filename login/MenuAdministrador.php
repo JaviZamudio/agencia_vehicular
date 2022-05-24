897588eda176b50d4fdc799eaa16e6cf772d4a0a
JaviZamudio <javier.elihu100@gmail.com>
2022-05-23 22:52:23 -0500

    dropdown menu added

--- login/MenuAdministrador.php
+++ login/MenuAdministrador.php
@@ -30,8 +30,8 @@
 						Ventas
 					</div>
 					<div class="dropdown-content">
-                        <a href="../Ventas/AdminVentas.php">Lista de Ventas</a>
-                        <a href="../Ventas/AdminRegistrarVentas.php">Nueva Venta</a>
+                        <a href="../Ventas/AdminVentas.php" class="dropdown-link">Lista de Ventas</a>
+                        <a href="../Ventas/AdminRegistrarVentas.php" class="dropdown-link">Nueva Venta</a>
 					</div>
 				</div>
 				<div class="dropdown">
@@ -39,8 +39,8 @@
 						Registros
 					</div>
 					<div class="dropdown-content">
-                        <a href="../Admin/Autos.php">Autos</a>
-                        <a href="../Admin/Empleados.php">Empleados</a>
+                        <a href="../Admin/Autos.php" class="dropdown-link">Autos</a>
+                        <a href="../Admin/Empleados.php" class="dropdown-link">Empleados</a>
 					</div>
 				</div>
 			</nav>
