b0acfbe21b2f99553fc54875fb39d6dfa14b3cf8
192mm <javier.18jun@gmail.com>
2022-05-24 22:03:46 -0500

    citas

--- Taller/AdminCitasTaller.php
+++ Taller/AdminCitasTaller.php
@@ -1,2 +1,141 @@
-//Agregar, eliminar y modificar?
-//O solamente agregar y modificar?
+<?php
+    session_start();
+    if(isset($_SESSION['Bandera']) && $_SESSION['Bandera']==1 && $_SESSION['Tipo']=="A"){
+?>
+<html> 
+<link rel="stylesheet" href="styles/style.css" type="text/css">
+    <label></label>
+    <head>
+		<title></title>
+		<style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#header {
+				margin: auto;
+				width: 700px;
+				font-family:Arial, Helvetica, sans-serif;
+
+			}
+			
+			ul, ol {
+				list-style:none;
+			}
+			
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:#000;
+				color:#fff;
+				text-decoration:none;
+				padding:10px 12px;
+				display:block;
+			}
+			
+			.nav li a:hover {
+				background-color:#434343;
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:-140px;
+				top:0px;
+			}
+			
+		</style>
+	</head>
+	<body>
+		<div id="header">
+			<ul class="nav">
+				<li><a href="">Taller</a>
+                    <ul>
+						<li><a href="../Taller/AdminCitasTaller.php">Citas</a></li>
+                        <li><a href="../Taller/AdminInvTaller.php">Inventario</a></li>
+                    </ul>
+
+                <li><a href="">Registros</a>
+                    <ul>
+						<li><a href="../Admin/Autos.php">Autos</a></li>
+                        <li><a href="../Admin/Empleados.php">Empleados</a></li>
+                    </ul>
+                <li><a href="">Ventas</a>
+                    <ul>
+						<li><a href="../Ventas/AdminVentas.php">Lista de Ventas</a></li>
+                        <li><a href="../Ventas/AdminRegistrarVentas.php">Nueva Venta</a></li>
+                    </ul>
+			</ul>
+		</div>
+        <br><br><br>
+        <div>
+        
+        </div>
+        <br><br><br>
+<?php
+        
+        include("./Conexiones.php");
+        $Con=Conectar();
+        $SQL="SELECT * FROM Inventario";
+        $Result=Ejecutar($Con,$SQL);
+        
+        ///Crear Tabla
+        echo '<table border=1><tr>';
+        echo '<th>'.'Id'.'</th>';
+        echo '<th>'.'Nombre de Usuario'.'</th>';
+        echo '<th>'.'Fecha de Entrada'.'</th>';
+        echo '<th>'.'Modelo'.'</th>';
+        echo '<th>'.'Marca'.'</th>';
+        echo '<th>'.'Placas'.'</th>';
+        echo '<th>'.'Nombre del Cliente'.'</th>';
+        echo '<th>'.'Tipo de Servicio'.'</th>';
+        echo '<th>'.'Contacto del cliente'.'</th>';
+        echo '<th>'.'Costo'.'</th>';
+
+        echo '</tr>';
+        for($F = 0; $F < mysqli_num_rows($Result); $F++)
+        {
+            $Fila=mysqli_fetch_row($Result);
+            echo '<tr>';
+            echo '<td>'.$Fila[0].'</td>';
+            echo '<td>'.$Fila[1].'</td>';
+            echo '<td>'.$Fila[2].'</td>';
+            echo '<td>'.$Fila[3].'</td>';
+            echo '<td>'.$Fila[4].'</td>'; 
+            echo '<td>'.$Fila[5].'</td>';
+            echo '<td>'.$Fila[6].'</td>';
+            echo '<td>'.$Fila[7].'</td>';
+            echo '<td>'.$Fila[8].'</td>';
+            echo '<td>'.$Fila[9].'</td>'; 
+            echo '<td>'.'<a href="EliminarCitas.php?Numero='.$Fila[0].'">Elimina</a>'.'</td>';
+            echo '<td>'.'<a href="ActualizarCitas.php?Numero='.$Fila[0].'">Actualizar</a>'.'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+        Cerrar($Con);    
+    }else{
+        // echo "<script>alert($_SESSION[''])</script>"
+        header("Location: ../login/FAcceso.html");
+    }
+
+?>
+
+        </div>
+	</body>
+</html>
--- Taller/CitasTaller.php
+++ Taller/CitasTaller.php
@@ -1,2 +1,54 @@
-//Agregar, eliminar y modificar?
-//O solamente agregar y modificar?
+<?php
+    session_start();
+    if(isset($_SESSION['Bandera']) && $_SESSION['Bandera']==1 && $_SESSION['Tipo']=="A"){
+?>
+
+<?php
+        
+        include("./Conexiones.php");
+        $Con=Conectar();
+        $SQL="SELECT * FROM Inventario";
+        $Result=Ejecutar($Con,$SQL);
+        
+        ///Crear Tabla
+        echo '<table border=1><tr>';
+        echo '<th>'.'Id'.'</th>';
+        echo '<th>'.'Nombre de Usuario'.'</th>';
+        echo '<th>'.'Fecha de Entrada'.'</th>';
+        echo '<th>'.'Modelo'.'</th>';
+        echo '<th>'.'Marca'.'</th>';
+        echo '<th>'.'Placas'.'</th>';
+        echo '<th>'.'Nombre del Cliente'.'</th>';
+        echo '<th>'.'Tipo de Servicio'.'</th>';
+        echo '<th>'.'Contacto del cliente'.'</th>';
+        echo '<th>'.'Costo'.'</th>';
+
+        echo '</tr>';
+        for($F = 0; $F < mysqli_num_rows($Result); $F++)
+        {
+            $Fila=mysqli_fetch_row($Result);
+            echo '<tr>';
+            echo '<td>'.$Fila[0].'</td>';
+            echo '<td>'.$Fila[1].'</td>';
+            echo '<td>'.$Fila[2].'</td>';
+            echo '<td>'.$Fila[3].'</td>';
+            echo '<td>'.$Fila[4].'</td>'; 
+            echo '<td>'.$Fila[5].'</td>';
+            echo '<td>'.$Fila[6].'</td>';
+            echo '<td>'.$Fila[7].'</td>';
+            echo '<td>'.$Fila[8].'</td>';
+            echo '<td>'.$Fila[9].'</td>'; 
+            echo '</tr>';
+        }
+        echo '</table>';
+        Cerrar($Con);    
+    }else{
+        // echo "<script>alert($_SESSION[''])</script>"
+        header("Location: ../login/FAcceso.html");
+    }
+
+?>
+
+        </div>
+	</body>
+</html>
